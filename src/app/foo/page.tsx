ba1a872dd7f4769d7d7e038d78d575ab7c9611a2
Sam Magura <srmagura@gmail.com>
2023-05-23 07:00:53 -0400

    it works

---
+++ src/app/foo/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Foo() {
+  return (
+    <main>
+      <h1>foo/page.tsx</h1>
+      <Link href="/">Go to Home</Link>
+    </main>
+  );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import Skeleton from "react-loading-skeleton";
+
+const loading = () => {
+  return (
+    <div>
+      <h1>loading.tsx</h1>
+      <Skeleton />
+    </div>
+  );
+};
+
+export default loading;
